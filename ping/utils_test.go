a559ee153599b6f9e63261167a45a72e1ef544e5
ioslh <ohislh@gmail.com>
2022-02-11 11:11:31 +0800

    chore: fix breaking util test

--- ping/utils_test.go
+++ ping/utils_test.go
@@ -1,6 +1,5 @@
 package ping
 
-
 import (
 	"testing"
 
@@ -11,32 +10,32 @@ func TestFormatIP(t *testing.T) {
 
 	Convey("IP", t, func() {
 		Convey("for v4 success", func() {
-			rc := FormatIP("192.168.0.1")
+			rc, _ := FormatIP("192.168.0.1")
 			So(rc, ShouldEqual, "192.168.0.1")
 		})
 
 		Convey("for v4 failure", func() {
-			rc := FormatIP("192.0.1")
+			rc, _ := FormatIP("192.0.1")
 			So(rc, ShouldEqual, "")
 		})
 
 		Convey("for v4 format", func() {
-			rc := FormatIP("[192.0.1.1] ")
+			rc, _ := FormatIP("[192.0.1.1] ")
 			So(rc, ShouldEqual, "192.0.1.1")
 		})
 
 		Convey("for v6 success", func() {
-			rc := FormatIP("[2002:ac1f:91c5:1::bd59]")
+			rc, _ := FormatIP("[2002:ac1f:91c5:1::bd59]")
 			So(rc, ShouldEqual, "[2002:ac1f:91c5:1::bd59]")
 		})
 
 		Convey("for v6 failure", func() {
-			rc := FormatIP("2002:ac1f:91c5:1:")
+			rc, _ := FormatIP("2002:ac1f:91c5:1:")
 			So(rc, ShouldEqual, "")
 		})
 
 		Convey("for v6 format", func() {
-			rc := FormatIP("2002:ac1f:91c5:1::bd59 ")
+			rc, _ := FormatIP("2002:ac1f:91c5:1::bd59 ")
 			So(rc, ShouldEqual, "[2002:ac1f:91c5:1::bd59]")
 		})
 	})
